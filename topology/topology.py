00369dd08dcf5bfe35a993f9bd20657e3d4fdf67
GANGULA SAIKIRAN REDDY <saikiranreddy19565@gmail.com>
2026-01-25 14:21:02 +0100

    Updated topology.py

--- topology/topology.py
+++ topology/topology.py
@@ -6,75 +6,95 @@ from mininet.node import RemoteController, OVSSwitch
 from mininet.cli import CLI
 from mininet.log import setLogLevel, info
 from mininet.link import TCLink
+import subprocess
+
+# Central veth map
+VETH_MAP = {
+    "h1": ("172.16.3.1/30", "172.16.3.2/30"),
+    "h3": ("172.16.4.1/30", "172.16.4.2/30"),
+    "h5": ("172.16.5.1/30", "172.16.5.2/30"),
+    "h7": ("172.16.7.1/30", "172.16.7.2/30"),  # SOC
+}
+
 
 class CustomTopology(Topo):
     """
     Custom SDN Topology with VLANs and Suricata IDS
-    - 1 Main switch (S1) connected to SDN controller and Suricata
-    - 4 Access switches (S2, S3, S4, S5)
-    - 8 Hosts (h1-h8)
-    - 1 Suricata IDS host
-    - VLAN1 (10.0.1.x): h1, h3, h5, h7
-    - VLAN2 (10.0.2.x): h2, h4, h6, h8
     """
-    
+
     def build(self):
-        # Add main switch S1
         s1 = self.addSwitch('s1', protocols='OpenFlow13')
-        
-        # Add access switches S2, S3, S4, S5
+
         s2 = self.addSwitch('s2', protocols='OpenFlow13')
         s3 = self.addSwitch('s3', protocols='OpenFlow13')
         s4 = self.addSwitch('s4', protocols='OpenFlow13')
         s5 = self.addSwitch('s5', protocols='OpenFlow13')
-        
-        # Add Suricata IDS host connected to S1
-        suricata = self.addHost('suricata', ip='10.0.0.100/24', mac='00:00:00:00:00:99')
-        
-        # Add hosts with VLAN-specific IP addressing
-        # VLAN1 (10.0.1.x) - h1, h3, h5, h7
-        h1 = self.addHost('h1', ip='10.0.1.1/24', mac='00:00:00:00:00:01')
-        h3 = self.addHost('h3', ip='10.0.1.3/24', mac='00:00:00:00:00:03')
-        h5 = self.addHost('h5', ip='10.0.1.5/24', mac='00:00:00:00:00:05')
-        h7 = self.addHost('h7', ip='10.0.1.7/24', mac='00:00:00:00:00:07')
-        
-        # VLAN2 (10.0.2.x) - h2, h4, h6, h8
-        h2 = self.addHost('h2', ip='10.0.2.2/24', mac='00:00:00:00:00:02')
-        h4 = self.addHost('h4', ip='10.0.2.4/24', mac='00:00:00:00:00:04')
-        h6 = self.addHost('h6', ip='10.0.2.6/24', mac='00:00:00:00:00:06')
-        h8 = self.addHost('h8', ip='10.0.2.8/24', mac='00:00:00:00:00:08')
-        
-        # Link S1 to Suricata IDS (MUST be first link to S1)
+
+        suricata = self.addHost('suricata', ip='10.0.0.100/24')
+
+        h1 = self.addHost('h1', ip='10.0.1.1/24')
+        h3 = self.addHost('h3', ip='10.0.1.3/24')
+        h5 = self.addHost('h5', ip='10.0.1.5/24')
+        h7 = self.addHost('h7', ip='10.0.1.7/24')
+
+        h2 = self.addHost('h2', ip='10.0.2.2/24')
+        h4 = self.addHost('h4', ip='10.0.2.4/24')
+        h6 = self.addHost('h6', ip='10.0.2.6/24')
+        h8 = self.addHost('h8', ip='10.0.2.8/24')
+
         self.addLink(s1, suricata)
-        
-        # Link S1 to access switches
+
         self.addLink(s1, s2)
         self.addLink(s1, s3)
         self.addLink(s1, s4)
         self.addLink(s1, s5)
-        
-        # Link hosts to access switches
-        # S2 connects to h1 (VLAN1), h2 (VLAN2)
+
         self.addLink(s2, h1)
         self.addLink(s2, h2)
-        
-        # S3 connects to h3 (VLAN1), h4 (VLAN2)
+
         self.addLink(s3, h3)
         self.addLink(s3, h4)
-        
-        # S4 connects to h5 (VLAN1), h6 (VLAN2)
+
         self.addLink(s4, h5)
         self.addLink(s4, h6)
-        
-        # S5 connects to h7 (VLAN1), h8 (VLAN2)
+
         self.addLink(s5, h7)
         self.addLink(s5, h8)
 
+
+def add_veth_to_host(net, host_name, host_ip, mn_ip):
+    """
+    Creates a veth pair between Node-RED (host OS)
+    and a Mininet host namespace.
+    """
+    host = net.get(host_name)
+    pid = host.pid
+
+    veth_host = f"veth-host-{host_name}"
+    veth_mn = f"veth-{host_name}"
+
+    subprocess.run(
+        ["ip", "link", "add", veth_host, "type", "veth", "peer", "name", veth_mn],
+        check=True
+    )
+
+    subprocess.run(["ip", "addr", "add", host_ip, "dev", veth_host], check=True)
+    subprocess.run(["ip", "link", "set", veth_host, "up"], check=True)
+
+    subprocess.run(
+        ["ip", "link", "set", veth_mn, "netns", str(pid)],
+        check=True
+    )
+
+    host.cmd(f"ip addr add {mn_ip} dev {veth_mn}")
+    host.cmd(f"ip link set {veth_mn} up")
+
+    info(f"*** veth configured for {host_name}\n")
+
+
 def startNetwork():
-    """Start the network with remote controller"""
     topo = CustomTopology()
-    
-    # Create network with remote controller
+
     net = Mininet(
         topo=topo,
         controller=lambda name: RemoteController(name, ip='127.0.0.1', port=6653),
@@ -82,84 +102,20 @@ def startNetwork():
         link=TCLink,
         autoSetMacs=True
     )
-    
+
     info('*** Starting network\n')
     net.start()
-    
-    info('*** Network Configuration:\n')
-    info('*** VLAN1 (10.0.1.x): h1, h3, h5, h7\n')
-    info('*** VLAN2 (10.0.2.x): h2, h4, h6, h8\n')
-    info('\n')
-    
-    # Find Suricata and S1
-    s1 = None
-    suricata = None
-    
-    for switch in net.switches:
-        if switch.name == 's1':
-            s1 = switch
-            break
-    
-    for host in net.hosts:
-        if host.name == 'suricata':
-            suricata = host
-            break
-    
-    if s1 and suricata:
-        # Find which port Suricata is connected to on S1
-        suricata_port = None
-        for intf in s1.intfList():
-            if hasattr(intf, 'link') and intf.link:
-                link = intf.link
-                if link.intf1.node == suricata or link.intf2.node == suricata:
-                    # Get the port number on S1 side
-                    if link.intf1.node == s1:
-                        suricata_port = s1.ports[link.intf1]
-                    else:
-                        suricata_port = s1.ports[link.intf2]
-                    break
-        
-        if suricata_port:
-            info(f'*** Suricata connected to S1 on port: {suricata_port}\n')
-            
-            # Store suricata port in a file for controller to read
-            try:
-                with open('/tmp/suricata_port.txt', 'w') as f:
-                    f.write(f'{s1.dpid},{suricata_port}')
-                info('*** Suricata port info saved to /tmp/suricata_port.txt\n')
-            except Exception as e:
-                info(f'*** Error saving Suricata port info: {e}\n')
-        else:
-            info('*** Warning: Could not determine Suricata port\n')
-    else:
-        info('*** Warning: Could not find S1 or Suricata\n')
-    
-    info('\n*** Switches:\n')
-    for switch in net.switches:
-        info(f'{switch.name} ')
-    info('\n\n')
-    
-    info('*** Hosts and their IPs:\n')
-    info('VLAN1:\n')
-    for host_name in ['h1', 'h3', 'h5', 'h7']:
-        host = net[host_name]
-        info(f'  {host.name}: {host.IP()}\n')
-    
-    info('VLAN2:\n')
-    for host_name in ['h2', 'h4', 'h6', 'h8']:
-        host = net[host_name]
-        info(f'  {host.name}: {host.IP()}\n')
-    
-    if suricata:
-        info(f'Suricata IDS: {suricata.IP()}\n')
-    info('\n')
-    
+
+    # loop-based setup
+    for host, (host_ip, mn_ip) in VETH_MAP.items():
+        add_veth_to_host(net, host, host_ip, mn_ip)
+
     info('*** Running CLI\n')
-    info('*** Type "help" for available commands\n')
     CLI(net)
-    
+
     info('*** Stopping network\n')
     net.stop()
+
 
 if __name__ == '__main__':
     setLogLevel('info')
